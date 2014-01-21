spulec/pep8ify

commit 664daf3c78c68935e472778bc52ee5d67fd0c03f

Fix regression that caused indentation of top level comments.
This fixes a regression caused by
904e380a58be5af77999e8fe5206a8019c8aa538.

Closes #16.

pep8ify/fixes/fix_whitespace_before_inline_comment.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 from lib2to3.fixer_base import BaseFix
+from .utils import node_text
+
+
+def get_previous_node(node):
+    """
+    Return the node before this node.
+    """
+    if node.prev_sibling:
+        return node.prev_sibling
+    return get_previous_node(node.parent)
 
 
 class FixWhitespaceBeforeInlineComment(BaseFix):
@@ -12,12 +22,23 @@ class FixWhitespaceBeforeInlineComment(BaseFix):
     '''
 
     def match(self, node):
-        if (node.prefix.count("#") and
-            not node.prefix.lstrip(" \t").startswith("\n")):
-            # If the node starts with a comment and not a newline, then it is
-            # an inline comment.
-            return True
-        return False
+        # An inline comment must contain with a #
+        if not node.prefix.count("#"):
+            return False
+
+        # If the node's prefix starts with a newline, then this is not an
+        # inline comment because there is not code before this.
+        if node.prefix.lstrip(" \t").startswith("\n"):
+            return False
+
+        # If the previous node ended in a newline, then this node is
+        # starting the line so it is not an inline comment.
+        prev_node = get_previous_node(node)
+        prev_node_text = node_text(prev_node)
+        if prev_node_text.endswith('\n'):
+            return False
+
+        return True
 
     def transform(self, node, results):
         position = node.prefix.find("#")

tests/fixtures/whitespace_before_inline_comment/whitespace_before_inline_comment3_in.py
@@ -1,0 +1,7 @@
+import baz
+
+baz.bar(
+    a='foobar',
+    # Comment between args
+    b='foobaz',
+)

tests/fixtures/whitespace_before_inline_comment/whitespace_before_inline_comment3_out.py
@@ -1,0 +1,7 @@
+import baz
+
+baz.bar(
+    a='foobar',
+    # Comment between args
+    b='foobaz',
+)

tests/fixtures/whitespace_before_inline_comment/whitespace_before_inline_comment2_in.py
@@ -1,0 +1,4 @@
+import foo
+# a comment
+
+foo.bar()

tests/fixtures/whitespace_before_inline_comment/whitespace_before_inline_comment2_out.py
@@ -1,0 +1,4 @@
+import foo
+# a comment
+
+foo.bar()

commit f581fe1dcc7198c6f93cfd7abb32516aa4b7401c

Fix regression caused by 664daf3c78c68935e472778bc52ee5d67fd0c03f. Pep8ify would crash in certain cases where comments did not have a previous node.
Closes #17.

pep8ify/fixes/fix_whitespace_before_inline_comment.py
@@ -9,7 +9,8 @@ def get_previous_node(node):
     """
     if node.prev_sibling:
         return node.prev_sibling
-    return get_previous_node(node.parent)
+    if node.parent:
+        return get_previous_node(node.parent)
 
 
 class FixWhitespaceBeforeInlineComment(BaseFix):
@@ -34,6 +35,9 @@ class FixWhitespaceBeforeInlineComment(BaseFix):
         # If the previous node ended in a newline, then this node is
         # starting the line so it is not an inline comment.
         prev_node = get_previous_node(node)
+        if not prev_node:
+            # If no previous node, this is not an inline comment.
+            return False
         prev_node_text = node_text(prev_node)
         if prev_node_text.endswith('\n'):
             return False

tests/fixtures/whitespace_before_inline_comment/whitespace_before_inline_comment3_in.py
@@ -1,3 +1,5 @@
+# comment 1
+# comment 2
 import baz
 
 baz.bar(

tests/fixtures/whitespace_before_inline_comment/whitespace_before_inline_comment3_out.py
@@ -1,3 +1,5 @@
+# comment 1
+# comment 2
 import baz
 
 baz.bar(